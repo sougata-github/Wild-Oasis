4d5d1d742b0a5ba440322502589330c77607c475
Sougata <sougatadas9874@gmail.com>
2024-01-16 12:54:42 +0530

    Build a resuable table component.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -11,18 +11,7 @@ import { useCreateCabin } from "./useCreateCabin";
 
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from "../../ui/Table";
 
 const Img = styled.img`
   display: block;
@@ -77,7 +66,7 @@ const CabinRow = ({ cabin }) => {
   }
 
   return (
-    <TableRow>
+    <Table.Row>
       <Img src={imageUrl} />
       <Cabin>{name}</Cabin>
       <div>Fits upto {maxCapacity} guests</div>
@@ -103,7 +92,7 @@ const CabinRow = ({ cabin }) => {
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
 
-          <Modal.Open name="delete">
+          <Modal.Open opens="delete">
             <button>
               <HiTrash />
             </button>
@@ -117,7 +106,7 @@ const CabinRow = ({ cabin }) => {
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 };
 
